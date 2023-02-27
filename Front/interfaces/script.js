cacb128a60d0c78f48e2cf111429ad54945c3d90
Alejandro Amezquita <alejo1996amez@gmail.com>
2023-02-27 11:21:56 -0500

    agregar funcionalidad de login

--- Front/interfaces/script.js
+++ Front/interfaces/script.js
@@ -1,6 +1,7 @@
-const username = document.getElementById('username')
-const password = document.getElementById('password')
-const button = document.getElementById('button')
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const button = document.getElementById('button');
+const url = "http://localhost:8081/app/";
 
 
 $( "#formCreateUser" ).submit(function( event ) {
@@ -12,8 +13,7 @@ $( "#formCreateUser" ).submit(function( event ) {
     });
     values.branchId = "1";
     delete values.email;
-    const url = "http://localhost:8081/app/users";
-    fetch(url, {
+    fetch(url + "users", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -21,6 +21,72 @@ $( "#formCreateUser" ).submit(function( event ) {
         body: JSON.stringify(values)
       })
         .then(response => response.json())
-        .then(data => console.log("usuario creado: ", data))
+        .then(data => {
+          alert("Usuario Creado");
+          console.log("Usuario Creado", data);
+        })
         .catch(error => console.error(error));
 });
+
+
+$( "#formLogin" ).submit(function( event ) {
+  event.preventDefault();
+  var inputs = $('#formLogin :input');
+  var values = {};
+  inputs.each(function() {
+      if(this.name != "") values[this.name] = $(this).val();
+  });
+  fetch(url + "login", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(values)
+    })
+      .then(response => response.json())
+      .then(data => 
+        {
+          console.log(data)
+          if(data.status){
+            sessionStorage.setItem("name", data.user.userName);
+            location.href = "http://localhost:8080/interfaces/home.html";
+          }
+          else{
+            sessionStorage.setItem("name", null);
+            alert("Error de autenticación!");
+          }
+        })
+      .catch(error => console.error(error));
+});
+
+function validateSession(){
+  let user = sessionStorage.getItem("name");
+  if(user){
+      console.log( "Welcome! " +  user);
+      $("#user").text(user);
+  }
+  else{
+      sessionStorage.clear();
+      location.href = "http://localhost:8080/interfaces/login.html";
+  }
+}
+
+function closeSession(){
+  sessionStorage.clear();
+  location.href = "http://localhost:8080/interfaces/login.html";
+}
+
+function getSession(){
+  fetch(url + "getSession", {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(data => 
+      {
+        console.log(data)
+      })
+    .catch(error => console.error(error));
+}
